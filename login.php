746e4bdac4fb0c4e3acffeefbd1b420dc5153c7e
ScottFiletti <59986177+ScottFiletti@users.noreply.github.com>
2020-01-17 18:47:14 -0500

    Version 1.1 - Adding include statement

--- login.php
+++ login.php
@@ -1,11 +1,11 @@
 <?php
-
+include("config.php");
 $x = $_REQUEST["x"];
 
 $object = (json_decode($x, true));
 
 echo $object->name;
-echo $object->user;
+echo $object->email;
 echo $object->pass; 
 
 //name is 0, user is 1, pass is 2
