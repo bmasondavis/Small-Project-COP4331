08eb054cd487b398e971cf73c39ea7365ba3cbc4
ScottFiletti <59986177+ScottFiletti@users.noreply.github.com>
2020-01-24 17:04:12 -0500

    Updated Create Account API

--- createaccount.php
+++ createaccount.php
@@ -1,16 +1,49 @@
 <?php
 include("config.php");
-$x = $_REQUEST["x"];
 
-$object = (json_decode($x, true));
+// Handling data in JSON format on the server-side using PHP
+header("Content-Type: application/json");
 
-echo $object->name;
-echo $object->email;
-echo $object->password; 
+// Decode the file given by our front end. 
+$data = json_decode(file_get_contents('php://input'), true);
+// $object = (json_decode($x, true));
+// echo $data["name"];
 
-//name is 0, user is 1, pass is 2
+// Get the username, email, and password from the database. 
+$woah = $data["name"];
+$waah = $data["email"];
+$eme = $data["password"];
+
+// Add the values into the database from the front end. 
+$sql = "INSERT INTO users (username, email, password) VALUES ('$woah','$waah','$eme')"; 
+$sql2 = "select * from users where username == '$woah'";
+
+// Query the database for an existing user. If there is no user, make an account. 
+$rows = $conn->query($sql2);
+
+// If there is no existing user, add it. 
+if (mysqli_num_rows($rows) < 1)
+{
+ // If this process worked....
+if ($conn->query($sql) === TRUE) 
+{
+
+    $new = array("New Account", $name); 
+    echo json_encode($new); 
+
+} 
+// Error checking for shenanigans. 
+else 
+{
+    $error = array("Error"); 
+    echo json_encode($error); 
+}
+}
+// If there is an existing account
+else
+{
+    //send back account already exists
+    echo json_encode("Account Already Exists"); 
+}
+$conn->close();
 ?>
-
-
-
-
