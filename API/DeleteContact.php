1e2d7e4d69fede6574ec2efc002411bdf1c38c97
collinpead <42709112+collinpead@users.noreply.github.com>
2020-02-03 14:04:50 -0500

    Updating to match AddContact formatting.

--- API/DeleteContact.php
+++ API/DeleteContact.php
@@ -7,20 +7,21 @@ include("config.php");
 // Decode the file given by our front end. 
 $data = json_decode(file_get_contents('php://input'), true);
 
-
 // Get the username, email, and password from the database. 
-$name = $data["name"];
-$email = $data["email"];
-$pass = $data["password"];
-$cid = $data["cid"]; 
+$fname = $data["firstname"];
+$lname = $data["lastname"];
+$phone = $data["phone"];
+$cemail = $data["cemail"];
+$uemail = $data["uemail"];
 
-$sql = "DELETE FROM contacts WHERE cid = '$cid'"; 
-
-// Is the rest of this code even needed? I can just return successful update. 
-$sql2 = "select * from contacts where cid = '$cid'";
-
-// Query the database for an existing user. If there is no user, make an account. 
+// Pulls the uid from the cookie provided user email.
+$sql2 = "select uid from users where email = '$uemail'";
 $rows = $conn->query($sql2);
+$result = mysqli_fetch_row($rows);
+$uid = $result[0];
+echo $uid;
+
+$sql = "DELETE FROM contacts WHERE uid = '$uid'"; 
 
 // If there is one user, a delete can be successful. 
 if (mysqli_num_rows($rows) === 1)
@@ -28,15 +29,12 @@ if (mysqli_num_rows($rows) === 1)
     // Delete the requested info from the database. 
     if ($conn->query($sql) === TRUE) 
     {
-
-    header('Content-type: application/json');
-    $success_delete = '{"error":0}';  
-    echo $success_delete; 
-
+    	header('Content-type: application/json');
+    	$success_delete = '{"error":0}';  
+    	echo $success_delete; 
     }
 }
-
-// If there isn't one user, there is a problem. 
+// If there is no user, there is a problem. 
 else
 {
     //send back account already exists
@@ -44,7 +42,5 @@ else
     $error_delete = '{"error":777}'; 
     echo $error_delete; 
 }
-
-
 $conn->close();
 ?>
