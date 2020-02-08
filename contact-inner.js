c89e6e13e56ff777c21001d3785b1924ff6ab4c6
nicolewhitee <54648883+nicolewhitee@users.noreply.github.com>
2020-02-07 20:31:13 -0500

    Add files via upload

--- contact-inner.js
+++ contact-inner.js
@@ -1,28 +1,4 @@
 // A function to open a contact already in the directory.
-
-document.getElementById("logout-btn").addEventListener('click', ()=> {
-window.location.href = 'index.html';
-});
-
-function dbQuery(value) {
-let search = {searchstring: value};
-let jsonObj = JSON.stringify(search);
-const xmlhttp = new XMLHttpRequest();
-
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       let responseObj = JSON.parse(xmlhttp.responseText);
-       console.log(xmlhttp.responseText);
-       if(responseObj.error === 202) alert("error code: 202");
-       else (responseObj.error === 0) ? window.location.href = 'google.com' : console.log("Account error");
-    }
-  }
-}
-
-function populate(contacts) {
-  let btn = document.createElement("BUTTON");
-}
-
 function clearFields() {
   document.getElementById("name").value = "";
   document.getElementById("phone").value = "";
@@ -30,29 +6,60 @@ function clearFields() {
 }
 
 function buttonControls() {
-document.getElementById("submit").addEventListener('click', () =>{
-    var fieldEmpty = "";
-  if(document.getElementById("name").value === "")  fieldEmpty += "Must fill Name field!  ";
-  if(document.getElementById("phone").value === "")  fieldEmpty += "Must fill Phone Number field!  ";
-  if(document.getElementById("email").value === "")  fieldEmpty += "Must fill Email field!  ";
-  (fieldEmpty === "") ? createContact(document.getElementById("name").value, document.getElementById("phone").value,
-   document.getElementById("email").value) : alert(fieldEmpty);
-  clearFields();
-});
+  document.getElementById("submit").addEventListener('click', () =>{
+      var fieldEmpty = "";
+    if(document.getElementById("name").value === "" && document.getElementById("phone") === "" &&
+       document.getElementById("email") === "")  fieldEmpty += "Please fill in all fields.";
+    (fieldEmpty === "") ? createContact(document.getElementById("name").value, document.getElementById("phone").value,
+     document.getElementById("email").value) : alert(fieldEmpty);
+    clearFields();
+  });
 
-document.getElementById("clear").addEventListener('click', ()=> clearFields());
+  document.getElementById("clear").addEventListener('click', ()=> clearFields());
+
+  document.getElementById("edit-submit").addEventListener('click', () =>{
+      var fieldEmpty = "";
+    if(document.getElementById("phone") === "" && document.getElementById("email") === "")
+      fieldEmpty += "Please fill in all fields.";
+    (fieldEmpty === "") ? createContact(document.getElementById("name").value, document.getElementById("phone").value,
+     document.getElementById("email").value) : alert(fieldEmpty);
+    clearFields();
+  });
+}
+
+function submit() {
+  var fieldEmpty = "";
+
+  if (document.getElementById("edit-phone") === "" || document.getElementById("edit-email") === "")
+    fieldEmpty += "Please fill in all fields.";
+
+  (fieldEmpty === "") ? createContact(contactName, document.getElementById("edit-phone").value,
+  document.getElementById("edit-email").value) : alert(fieldEmpty);
+  clearFields();
 }
 
 function openContact(contactName) {
-  var i, x, tabcontent, tablinks;
+  var i, x, y, z, m, tabcontent, tablinks;
   // Display edit and delete buttons.
   document.getElementById('delete').style.display = "block";
   document.getElementById('edit').style.display = "block";
+
+  /*
+  y = document.getElementById('welcome-page');
+  y.style.display = "none";
+  */
+
+  z = document.getElementById('overlay');
+  z.style.display = "none";
+
+  m = document.getElementById("editPanel");
+  m.style.display = "none";
 
 	// Hide the "add new contact" page
 	x = document.getElementById('contactPanel');
 	x.style.display = 'none';
   let contact = document.getElementById("contactInfo");
+  console.log(contactName);
   document.getElementById("contactHeader").innerHTML = contactName;
 	contact.style.display = "block";
   document.getElementById("contactBtns").style.display = "block";
@@ -60,11 +67,20 @@ function openContact(contactName) {
 
 // A function to add a contact to the directory.
 function addContact() {
-	var tabcontent, x = document.getElementById("contactPanel");
+	var tabcontent, x = document.getElementById("contactPanel"), y, z, m;
   buttonControls();
   // Hide edit and delete buttons.
   document.getElementById('delete').style.display = "none";
   document.getElementById('edit').style.display = "none";
+
+  y = document.getElementById('welcome-page');
+  y.style.display = "none";
+
+  z = document.getElementById("overlay");
+  z.style.display = "none";
+
+  m = document.getElementById("editPanel");
+  m.style.display = "none";
 
 	tabcontent = document.getElementsByClassName("tabcontent");
 
@@ -80,13 +96,13 @@ function addContact() {
 }
 
 function createContact(contactName, phone, email) {
- let newA = document.createElement("a");
- let newLi = document.createElement("li");
- newA.innerHTML = contactName;
- newA.setAttribute('href', '#');
- newLi.appendChild(newA);
- newLi.addEventListener('click', ()=> openContact(contactName));
- document.getElementById("tablinks").appendChild(newLi);
+  let newA = document.createElement("a");
+  let newLi = document.createElement("li");
+  newA.innerHTML = contactName;
+  newA.setAttribute('href', '#');
+  newLi.appendChild(newA);
+  newLi.addEventListener('click', ()=> openContact(contactName));
+  document.getElementById("tablinks").appendChild(newLi);
 }
 
 // A function to search for contacts in the directory.
@@ -105,5 +121,28 @@ function searchFunction() {
     } else {
       li[i].style.display = "none";
     }
+  }
+}
+
+function editContact() {
+  var x = document.getElementById("editPanel"), y;
+
+  buttonControls();
+  // Hide edit and delete buttons.
+  document.getElementById('delete').style.display = "none";
+  document.getElementById('edit').style.display = "none";
+  tabcontent = document.getElementsByClassName("tabcontent");
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  y = document.getElementById("name").innerHTML;
+  document.getElementById("edit-name").placeholder = y;
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 }
