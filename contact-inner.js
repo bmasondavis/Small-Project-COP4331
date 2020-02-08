f802e4b32f06cd831b36749194080a83a27725c4
jediburrito <jediburrito@gmail.com>
2020-02-07 22:04:02 -0500

    update to check array

--- contact-inner.js
+++ contact-inner.js
@@ -14,8 +14,8 @@ const xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       console.log(xmlhttp.responseText);
-       let responseObj = JSON.parse(xmlhttp[0].responseText);
-       console.log(responseObj.responseText);
+       let responseObj = JSON.parse(xmlhttp.responseText);
+       console.log(responseObj[0].responseText);
     }
   }
   xmlhttp.open("POST", "fetchContacts.php", true);
