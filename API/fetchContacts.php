bb173e16b7e57c9b444109032b8cd17fbf4dfad5
bmasondavis <54715795+bmasondavis@users.noreply.github.com>
2020-02-07 21:39:12 -0500

    Slurp

--- API/fetchContacts.php
+++ API/fetchContacts.php
@@ -33,8 +33,8 @@ if (mysqli_num_rows($rows) === 1)
     $contacts[] = $row;
   }
   header('Content-type: application/json');
-  json_encode($contacts);
-  echo $contacts;
+  $json_contacts = json_encode($contacts);
+  echo $json_contacts;
   }
   // Delete the requested info from the database.
 
