f7a5473740d6517038d534061778e98cfbaae54d
jediburrito <jediburrito@gmail.com>
2020-02-09 19:29:13 -0500

    retraction

--- contact-inner.js
+++ contact-inner.js
@@ -241,7 +241,7 @@ function updateContact(newContact){
   createContact(newContact);
 let contact = document.getElementById(newContact.cid);
 contact.innerHTML = newContact.firstName;
-findContact(newContact.cid) = newContact;
+findContact(newContact) = newContact;
 }
 
 //show editContact page
