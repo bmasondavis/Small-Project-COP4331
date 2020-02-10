41204e5530d9b43bdf87ea2b9b5bc33b771f016a
jediburrito <jediburrito@gmail.com>
2020-02-10 02:27:33 -0500

    front end overhaul

--- contact-inner.js
+++ contact-inner.js
@@ -48,23 +48,19 @@ function buttonControls() {
 
   document.getElementById("edit-submit").addEventListener('click', () =>{
       var fieldEmpty = "";
-    if(document.getElementById("edit-firstName").value === "" || document.getElementById("edit-lastName").value === "" || document.getElementById("edit-phone").value === "" ||
-       document.getElementById("edit-email").value === "")  console.log("Please fill in all fields.");
+    if(document.getElementById("firstName").value === "" || document.getElementById("lastName").value === "" || document.getElementById("phone").value === "" ||
+       document.getElementById("email").value === "")  console.log("Please fill in all fields.");
       else{
-    newContact =  {firstName: document.getElementById("edit-firstName").value, lastName: document.getElementById("edit-lastName").value,
-    phone: document.getElementById("edit-phone").value, cemail: document.getElementById("edit-email").value, uemail: Cookies.get("emailID"), cid: thisCid};
+    newContact =  {firstName: document.getElementById("firstName").value, lastName: document.getElementById("lastName").value,
+    phone: document.getElementById("edit-phone").value, cemail: document.getElementById("email").value, uemail: Cookies.get("emailID"), cid: thisCid};
     dbContactEdit(newContact);
     clearFields();
   }
   });
 }
 
-function populate(value, email){
-  clearCache();
-  populateBar(value, email);
-}
 //function to populate sidebar and load in Cache
-function populateBar(value, email) {
+function populate(value, email) {
 let search = {uemail: email, searchstring: value};
 let jsonObj = JSON.stringify(search);
 const xmlhttp = new XMLHttpRequest();
@@ -72,7 +68,7 @@ const xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        let responseObj = JSON.parse(xmlhttp.responseText);
-       
+       clearCache();
        for(let i = 0; i < responseObj.length; i++) {
         cache.push(responseObj[i]);
        createContact(responseObj[i]);
@@ -139,61 +135,51 @@ function findContact(cid) {
     if(cache[i].cid = cid) return cache[i];
 }
 
-function openContact(newContact, cid) {
-  var i, x, y, z, m, tabcontent, tablinks;
+function openContact(cid) {
   // Display edit and delete buttons.
-  document.getElementById('delete').style.display = "block";
-  document.getElementById('edit').style.display = "block";
   thisCid = cid;
-  contactInfo = findContact(cid);
-  /*
-  y = document.getElementById('welcome-page');
-  y.style.display = "none";
-  */
-
-  z = document.getElementById('overlay');
-  z.style.display = "none";
-
-  m = document.getElementById("editPanel");
-  m.style.display = "none";
-
+  contact = findContact(cid);
+  
+  document.getElementById('overlay').stlye.display = "none";
+  buttonControls();
 	// Hide the "add new contact" page
-	x = document.getElementById('contactPanel');
-	x.style.display = 'none';
-  let contact = document.getElementById("contactInfo");
-  document.getElementById("contactHeader").innerHTML = newContact.firstName + " " + newContact.lastName;
-	contact.style.display = "block";
-  document.getElementById("contactBtns").style.display = "block";
+	document.getElementById('contactPanel').style.display = "block";
+  document.getElementById("firstName").value = contact.firstName;
+  document.getElementById("lastName").value = contact.lastName;
+  document.getElementById("phone").value = contact.phone;
+  document.getElementById("email").value = contact.email;
+  //set to read only
+  document.getElementById("firstName").readOnly = true;
+  document.getElementById("lastName").readOnly = true;
+  document.getElementById("phone").readOnly = true;
+  document.getElementById("email").readOnly = true;
+  //set button visibility
+  document.getElementById("contactPanel").style.display = "block";
+  document.getElementById("addContact-btns").style.display = "none";
+  document.getElementById("panel-btns").style.display = "none";
+  document.getElementById("contact-btns").style.display = "block";
 }
 
 // A function to add a contact to the directory.
 function addContact() {
 	var tabcontent, x = document.getElementById("contactPanel"), y, z, m;
   buttonControls();
-  // Hide edit and delete buttons.
-  document.getElementById('delete').style.display = "none";
-  document.getElementById('edit').style.display = "none";
-
-  y = document.getElementById('welcome-page');
-  y.style.display = "none";
-
-  z = document.getElementById("overlay");
-  z.style.display = "none";
-
-  m = document.getElementById("editPanel");
-  m.style.display = "none";
-
-	tabcontent = document.getElementsByClassName("tabcontent");
-
-	for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-	if (x.style.display === "none") {
-		x.style.display = "block";
-	} else {
-		x.style.display = "none";
-	}
+  
+  document.getElementById('contactPanel').style.display = "block";
+  document.getElementById("firstName").value = "";
+  document.getElementById("lastName").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("email").value = "";
+  //set to read only
+  document.getElementById("firstName").readOnly = false;
+  document.getElementById("lastName").readOnly = false;
+  document.getElementById("phone").readOnly = false;
+  document.getElementById("email").readOnly = false;
+  //set button visibility
+  document.getElementById("contactPanel").style.display = "block";
+  document.getElementById("addContact-btns").style.display = "block";
+  document.getElementById("panel-btns").style.display = "none";
+  document.getElementById("contact-btns").style.display = "none";
 }
 
 //function to create contact on sidebar
@@ -245,27 +231,27 @@ findContact(newContact) = newContact;
 
 //show editContact page
 function editContact() {
-  var x = document.getElementById("editPanel"), y;
+  
   let contact = findContact(thisCid);
-  console.log(contact);
-  buttonControls();
   // Hide edit and delete buttons.
   document.getElementById('delete').style.display = "none";
   document.getElementById('edit').style.display = "none";
   tabcontent = document.getElementsByClassName("tabcontent");
 
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+  for (i = 0; i < tabcontent.length; i++) tabcontent[i].style.display = "none";
 
-  document.getElementById("edit-firstName").value = contact.firstName;
-  document.getElementById("edit-lastName").value = contact.lastName;
-  document.getElementById("edit-phone").value = contact.phone;
-  document.getElementById("edit-email").value = contact.email;
+  document.getElementById("firstName").value = contact.firstName;
+  document.getElementById("lastName").value = contact.lastName;
+  document.getElementById("phone").value = contact.phone;
+  document.getElementById("email").value = contact.email;
 
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+  document.getElementById("firstName").readOnly = true;
+  document.getElementById("lastName").readOnly = true;
+  document.getElementById("firstName").readOnly = false;
+  document.getElementById("lastName").readOnly = false;
+
+  document.getElementById("contactPanel").style.display = "block";
+  document.getElementById("addContact-btns").style.display = "none";
+  document.getElementById("panel-btns").style.display = "blocks";
+  document.getElementById("contact-btns").style.display = "none";
 }
