ca1d0c8962db809ceaa24607f1df7a4d33dde3e6
bmasondavis <54715795+bmasondavis@users.noreply.github.com>
2020-02-11 02:37:29 -0500

    verify password with the hash before pulling email

--- login.php
+++ login.php
@@ -11,6 +11,10 @@ $data = json_decode(file_get_contents('php://input'), true);
 $email = $data["email"];
 $pass = $data["password"];
 
+$sql3 = "select password from users where email = '$email'";
+$hash = $conn->query($sql3);
+
+
 // Find the email and password in the database
 
 // Backup
@@ -18,27 +22,29 @@ $pass = $data["password"];
 //$sql = "select * from users where email = '$email'";
 //$sql2 = "select * from password where password = '$pass'";
 
-$sql = "select * from users where email = '$email' && password = '$pass'";
+$sql = "select * from users where email = '$email' && password = '$hash'";
 
 
 // Check the emails and pass. 
 $row = $conn->query($sql);
 
-// If the user matches what is in the database, return the email. 
-if(mysqli_num_rows($row) === 1)
-{
+// If the user matches what is in the database, return the email.
+if (password_verify($password, $hash)
+    {
+    if(mysqli_num_rows($row) === 1)
+        {
     
-    header("Content-Type: application/json");
-    // This is for creating a successful account
-    $success = '{"email":"' . $email . '", "password":"' . $pass . '", "error":0}';
-    echo $success; 
+             header("Content-Type: application/json");
+               // This is for correct credentials, successful login
+             $success = '{"email":"' . $email . '", "password":"' . $pass . '", "error":0}';
+             echo $success; 
 
-}
+        }
+    }
 
 // If it doesn't exist, then the user and password was incorrect. 
 else
 {
-    
     header("Content-Type: application/json");
     // Invalid Credentials recieved. 
     $failure = '{"email":"' . $email . '", "password":"' . $pass . '", "error":302}';
